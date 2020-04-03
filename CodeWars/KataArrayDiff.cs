0c32c5eece08569fc575e8e0d4da378859f8dc56
andrewmysaka@gmail.com <andrewmysaka@gmail.com>
2020-04-03 17:16:58 +0300

    ArrayDiff

---
+++ CodeWars/KataArrayDiff.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars
+{
+    partial class Program
+    {
+        public class KataArrayDiff
+        {
+            public static int[] ArrayDiff(int[] a, int[] b)
+            {
+                return a.Where(el => !b.Contains(el)).ToArray();
+            }
+        }
+    }
+}
---
+++ CodeWars/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeWars
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            KataArrayDiff.ArrayDiff(new int[] { 1, 2 }, new int[] { 1 }); 
+        }
+    }
+}

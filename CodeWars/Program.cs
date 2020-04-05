501bd27572d5b9741d639fef51eb4c2dbf6d59a4
andrewmysaka@gmail.com <andrewmysaka@gmail.com>
2020-04-05 21:33:27 +0300

    FirstNonConsecutive and IsHeGonnaSurvive

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -14,8 +14,9 @@ namespace CodeWars
             // RemoveEveryOther.RemoveEveryOther8(new object[] { "Hello", "Goodbye", "Hello Again"});
             //  Console.WriteLine("hello 133wOrld".ToAlternatingCase8());
             // Console.WriteLine("HELLO I AM DONALD".IsUpperCase8());
-            Console.WriteLine(ArrayMadness.ArrayMadness8(new int[] {4, 5, 6}, new int[] {1, 2, 3}));
-            
+            // Console.WriteLine(ArrayMadness.ArrayMadness8(new int[] {4, 5, 6}, new int[] {1, 2, 3}));
+            // Console.WriteLine(FirstNonConsecutive.FirstNonConsecutive8(new int[] {1, 2, 3, 4, 6, 7, 8}));
+            // Console.WriteLine(IsHeGonnaSurvive.Hero(10,5));
         }
     }
 }

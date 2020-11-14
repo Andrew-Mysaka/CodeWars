0071822f71b3c5328cf8d3038d1b7e9b0c16e9da
andrewmysaka@gmail.com <andrewmysaka@gmail.com>
2020-11-14 22:24:48 +0200

    kata 7

---
+++ CodeWars/Kata 7 kyu/TwoToOne.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars.Kata_7_kyu
+{
+    class TwoToOne
+    {
+        public static string Longest(string s1, string s2)
+        {
+            string temp = s1 + s2;
+            List<char> strList = new List<char>();
+
+            foreach (var letter in temp)
+            {
+                if (!strList.Contains(letter))
+                    strList.Add(letter);
+            }
+            strList.Sort();
+
+            string result = "";
+            foreach (var chr in strList)
+                result += chr;
+
+            return result;
+
+            //string.Concat((s1 + s2).Distinct().OrderBy(x => x));
+        }
+    }
+    //Take 2 strings s1 and s2 including only letters from ato z.Return a new sorted string,
+    //the longest possible, containing distinct letters,
+
+    //each taken only once - coming from s1 or s2.
+}

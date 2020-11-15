36e35218a5897fdd6e0b387e3bfdf891d00e4c7c
andrewmysaka@gmail.com <andrewmysaka@gmail.com>
2020-11-15 21:48:53 +0200

    kata 7

---
+++ CodeWars/Kata 7 kyu/ArrayLeaders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars.Kata_7_kyu
+{
+    class ArrayLeaders
+    {
+        public static int[] ArrayLeader(int[] numbers)
+        {
+            List<int> results = new List<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int sum = 0;
+                for (int j = i+1; j < numbers.Length; j++)
+                {
+                    sum += numbers[j];
+                }
+
+                if (numbers[i] > sum)
+                {
+                    results.Add(numbers[i]);
+                }
+            }
+            
+            return results.ToArray();
+        }
+        // public static int[] ArrayLeaders(int[] n) => n.Where((x, i) => x > n.Skip(i + 1).Sum()).ToArray();
+    }
+    // An element is leader if it is greater than The Sum all the
+    // elements to its right side.
+}
--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -28,6 +28,7 @@ namespace CodeWars
             // Kata_7_kyu.ListFiltering.GetIntegersFromList(new List<object>() { 1, 2, "a", "b", "aasf", "1", "123", 231 });
             // Console.WriteLine(Kata_7_kyu.TwoToOne.Longest("xyaabbbccccdefww", "xxxxyyyyabklmopq"));
             // Console.WriteLine(Kata_7_kyu.AutomorphicNumber.Automorphic(6));
+            Kata_7_kyu.ArrayLeaders.ArrayLeader(new int[] { 0, -1, -29, 3, 2 });
         }
     }
 }
